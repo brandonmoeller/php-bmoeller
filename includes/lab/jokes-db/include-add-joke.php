e78263f221c85546d8679265fa3a17a0b29649ce
brandonmoeller <brandonmoeller@yahoo.com>
2022-09-23 20:48:11 -0500

    LAB/JOKES-DB - Mysqli_error and addslashes - From https://www.youtube.com/watch?v=4ZqObvQXYAQ&list=PLhPyEFL5u-i0zEaDF0IPLYvm8zOKnz70r&index=10

--- includes/lab/jokes-db/include-add-joke.php
+++ includes/lab/jokes-db/include-add-joke.php
@@ -5,11 +5,14 @@ require_once($_SERVER['DOCUMENT_ROOT'] . '/includes/lab/jokes-db/include-db-conn
 $new_joke_question = $_GET["newjoke"];
 $new_joke_answer = $_GET["newanswer"];
 
+$new_joke_question = addslashes($new_joke_answer);
+$new_joke_answer = addslashes($new_joke_answer);
+
 // 
 echo "<h2>Trying to add a new joke: $new_joke_question and $new_joke_answer</h2>";
 $sql = "INSERT INTO Jokes_table (JokeID, Joke_question, Joke_answer) VALUES (NULL, '$new_joke_question', '$new_joke_answer')";
 
-$result = $mysqli->query($sql);
+$result = $mysqli->query($sql) or die(mysqli_error($mysqli));
 
 require_once($_SERVER['DOCUMENT_ROOT'] . '/includes/lab/jokes-db/include-search-all-jokes.php');
 
