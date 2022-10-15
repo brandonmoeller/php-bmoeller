d756f9990486984437ce8b976d94b856a828e244
brandonmoeller <63176993+brandonmoeller@users.noreply.github.com>
2022-10-15 11:05:38 -0500

    LAB/OOP - Continuing w3 PHP Tutorial: PHP OOP

--- lab/oop/index.php
+++ lab/oop/index.php
@@ -245,7 +245,7 @@ require_once($_SERVER['DOCUMENT_ROOT'] . '/includes/functions.php'); // This cal
 //
 //
 
-// PHP OOP Constants
+// PHP OOP Class Constants
 // Following along here: https://www.w3schools.com/php/php_oop_constants.asp
 
 // class Goodbye {
@@ -254,15 +254,135 @@ require_once($_SERVER['DOCUMENT_ROOT'] . '/includes/functions.php'); // This cal
 
 // echo Goodbye::LEAVING_MESSAGE;
 
-class Goodbye {
-  const LEAVING_MESSAGE = "Thank you for visiting W3Schools.com!";
-  public function byebye() {
-    echo self::LEAVING_MESSAGE;
-  }
-}
+// class Goodbye {
+//   const LEAVING_MESSAGE = "Thank you for visiting W3Schools.com!";
+//   public function byebye() {
+//     echo self::LEAVING_MESSAGE;
+//   }
+// }
 
-$goodbye = new Goodbye();
-$goodbye->byebye();
+// $goodbye = new Goodbye();
+// $goodbye->byebye();
+
+//
+//
+//
+
+// PHP OOP Abstract Classes
+// Following along here: https://www.w3schools.com/php/php_oop_classes_abstract.asp
+
+// abstract class ParentClass {
+//   abstract public function someMethod1();
+//   abstract public function someMethod2($name, $color);
+//   abstract public function someMethod3() : string;
+// }
+
+// Example
+
+// Parent class
+// abstract class Car {
+//   public $name;
+//   public function __construct($name) {
+//     $this->name = $name;
+//   }
+//   abstract public function intro() : string; 
+// }
+
+// // Child classes
+// class Audi extends Car {
+//   public function intro() : string {
+//     return "Choose German quality! I'm an $this->name!"; 
+//   }
+// }
+
+// class Volvo extends Car {
+//   public function intro() : string {
+//     return "Proud to be Swedish! I'm a $this->name!"; 
+//   }
+// }
+
+// class Citroen extends Car {
+//   public function intro() : string {
+//     return "French extravagance! I'm a $this->name!"; 
+//   }
+// }
+
+// // Create objects from the child classes
+// $audi = new Audi("Audi");
+// echo $audi->intro();
+// echo "<br>";
+
+// $volvo = new Volvo("Volvo");
+// echo $volvo->intro();
+// echo "<br>";
+
+// $citroen = new Citroen("Citroen");
+// echo $citroen->intro();
+
+// More Abstract Class Examples
+// An example where the abstract method has an argument:
+
+// abstract class ParentClass
+// {
+//   // Abstract method with an argument
+//   abstract protected function prefixName($name);
+// }
+
+// class ChildCLass extends ParentClass {
+//   public function prefixName($name) {
+//     if ($name == "John Doe") {
+//       $prefix = "Mr.";
+//     } elseif ($name == "Jane Doe") {
+//       $prefix = "Mrs.";
+//     } else {
+//       $prefix = "Yer Majesty";
+//     }
+//     return "{$prefix} {$name}";
+//   }
+// }
+
+// $class = new ChildClass;
+// echo $class->prefixName("John Doe");
+// echo "<br>"; 
+// echo $class->prefixName("Jane Doe");
+
+//
+
+// Another example where the abstract method has an argument, and the child class has two optional arguments that are not defined in the parent's abstract method:
+
+// abstract class ParentClass
+// {
+//   // Abstract method with an argument
+//   abstract protected function prefixName($name);
+// }
+
+// class ChildCLass extends ParentClass
+// {
+//   // The child class may define optional arguments that are not in the parent's abstract method
+//   public function prefixName($name, $separator = ".", $greet = "Dear")
+//   {
+//     if ($name == "John Doe") {
+//       $prefix = "Mr";
+//     } elseif ($name == "Jane Doe") {
+//       $prefix = "Mrs";
+//     } else {
+//       $prefix = "";
+//     }
+//     return "{$greet} {$prefix}{$separator} {$name}";
+//   }
+// }
+
+// $class = new ChildClass;
+// echo $class->prefixName("John Doe");
+// echo "<br>"; 
+// echo $class->prefixName("Jane Doe");
+
+
+
+
+
+
+
 
 ?>
 
