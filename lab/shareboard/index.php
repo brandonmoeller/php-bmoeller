2dd10d65d03c8f78b6674c14b7d6b53c10f5898c
brandonmoeller <63176993+brandonmoeller@users.noreply.github.com>
2022-10-23 12:17:09 -0500

    LAB/SHAREBOARD - Created controllers - Eduonix: OOP PHP Complete Website (6:25)

--- lab/shareboard/index.php
+++ lab/shareboard/index.php
@@ -1,8 +1,18 @@
 <?php
+// Following along Section 6, #25: Creating Controllers - https://www.eduonix.com/new_dashboard/learn-object-oriented-php-by-building-a-complete-website
+
 // Include Config
 require('config.php');
 
 require('classes/Bootstrap.php');
+require('classes/Controller.php');
+
+require('controllers/home.php');
+require('controllers/shares.php');
+require('controllers/users.php');
 
 $bootstrap = new Bootstrap($_GET);
 $controller = $bootstrap->createController();
+if($controller){
+  $controller->executeAction();
+}
