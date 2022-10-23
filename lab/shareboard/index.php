9aa9dc896eff7ac8c010e11e44503439b472fc79
brandonmoeller <63176993+brandonmoeller@users.noreply.github.com>
2022-10-23 11:07:46 -0500

    LAB/SHAREBOARD - Continuing Section 6 of Eduonix: OOP PHP Complete Website by capitalizing class files

--- lab/shareboard/index.php
+++ lab/shareboard/index.php
@@ -2,7 +2,7 @@
 // Include Config
 require('config.php');
 
-require('classes/bootstrap.php');
+require('classes/Bootstrap.php');
 
 $bootstrap = new Bootstrap($_GET);
 $controller = $bootstrap->createController();
